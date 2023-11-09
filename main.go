c299861d18190ed9079fa7ac0915c67d30dca847
Hanan Asyrawi Rivai <hanan.rivai@bitcoin.co.id>
2023-11-10 01:54:00 +0700

    update

--- main.go
+++ main.go
@@ -6,10 +6,16 @@ import (
 	"log"
 
 	firebase "firebase.google.com/go"
+	"github.com/go-resty/resty/v2"
 )
 
 func main() {
 	ctx := context.Background()
+
+	lr, err := SendRequestLogin("hasyrawi@gmail.com", "awiroot123")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	app, err := firebase.NewApp(context.Background(), nil)
 
@@ -22,36 +28,73 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
-	ur, err := c.GetUserByEmail(ctx, "hanan@asyrawih.id")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	token, err := c.CustomTokenWithClaims(ctx, ur.UID, map[string]interface{}{
-		"user":    "hanan",
-		"user_id": "test",
+	s, err := c.CustomTokenWithClaims(ctx, lr.LocalID, map[string]interface{}{
+		"user_id":     lr.LocalID,
+		"email":       lr.Email,
+		"device_info": "some device info in here",
 	})
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err.Error())
 	}
 
-	//
-	// ❮ curl 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyCUiotIte0FYHOK3Zo9h44Qwyo1OtVFlfI' \
-	//       -H 'Content-Type: application/json' \
-	//       --data-binary '{"token":"custom_token_from_backend_generated","returnSecureToken":true}'
+	token, err := ExchangeCustomIdToken(s)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 
-	t, err := c.VerifyIDToken(
-		ctx,
-		"eyJhbGciOiJSUzI1NiIsImtpZCI6ImQ0OWU0N2ZiZGQ0ZWUyNDE0Nzk2ZDhlMDhjZWY2YjU1ZDA3MDRlNGQiLCJ0eXAiOiJKV1QifQ.eyJ1c2VyIjoiaGFuYW4iLCJ1c2VyX2lkIjoiSDVRQ2hIeXZLZWFrbHFTYVNSaWEweFpUS0k0MiIsImlzcyI6Imh0dHBzOi8vc2VjdXJldG9rZW4uZ29vZ2xlLmNvbS9kYW50ZS02NjYiLCJhdWQiOiJkYW50ZS02NjYiLCJhdXRoX3RpbWUiOjE2OTk1NDc3MjcsInN1YiI6Ikg1UUNoSHl2S2Vha2xxU2FTUmlhMHhaVEtJNDIiLCJpYXQiOjE2OTk1NDc3MjcsImV4cCI6MTY5OTU1MTMyNywiZW1haWwiOiJoYW5hbkBhc3lyYXdpaC5pZCIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJoYW5hbkBhc3lyYXdpaC5pZCJdfSwic2lnbl9pbl9wcm92aWRlciI6ImN1c3RvbSJ9fQ.V7S6q3o-1MHOMO0HC7Ya1kOArOQ7KUSbmjJ_2f8EkXDp8ueD1kKN_q3OiQnzOjRl8TUfJqzJoAyyFj4r7kV2FS-VD3OmeQ5CarOH1NrKq6MNGvyBgmIuxPQxj_4igwfX4FUMLymFEyIDWbv9MD50R0bY3w1Qr3BslSbgrG89lLMjcK3WqVdNUYadWgCO8UCQH_WuLIXdf9cNIQ2Frc26yzDka5f2V5PFXI7mW0t7s0rZshXpV2Z-5sk6oDHHsuEbEowS-obrAvlxo9X7Kw41n1oOkYPITPP-TUW49mN05L2yXKPafFJCC3lW6G89hp_tVD1hWpuYxS30iySq8kVBEQ",
-	)
-
+	// verify id token with revocetion to tell is not has revoked
+	t, err := c.VerifyIDTokenAndCheckRevoked(ctx, token.IDToken)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("t.Claims: %v\n", t.Claims["user"])
+	fmt.Printf("t: %+v\n", t)
 
-	fmt.Printf("token: %v\n", token)
+}
 
+type LoginResponse struct {
+	Kind         string `json:"kind"`
+	LocalID      string `json:"localId"`
+	Email        string `json:"email"`
+	DisplayName  string `json:"displayName"`
+	IDToken      string `json:"idToken"`
+	Registered   bool   `json:"registered"`
+	RefreshToken string `json:"refreshToken"`
+	ExpiresIn    string `json:"expiresIn"`
+}
+
+var client = resty.New().R()
+
+// Send Request login to identity-toolkit api
+func SendRequestLogin(email, password string) (*LoginResponse, error) {
+	result := new(LoginResponse)
+	_, err := client.SetHeader("content-type", "application/json").
+		SetBody(map[string]interface{}{
+			"email":    email,
+			"password": password,
+		}).
+		SetResult(result).
+		Post("https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCUiotIte0FYHOK3Zo9h44Qwyo1OtVFlfI")
+
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+// Send Request login to identity-toolkit api
+func ExchangeCustomIdToken(token string) (*LoginResponse, error) {
+	result := new(LoginResponse)
+	_, err := client.SetHeader("content-type", "application/json").
+		SetBody(map[string]interface{}{
+			"token":             token,
+			"returnSecureToken": true,
+		}).
+		SetResult(result).
+		Post("https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyCUiotIte0FYHOK3Zo9h44Qwyo1OtVFlfI")
+
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
 }
